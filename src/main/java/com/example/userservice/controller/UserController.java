521d34a11141867472901cb1e370e992129b8dcd
seokrae <kslbsh@gmail.com>
2021-07-19 19:19:34 +0900

    feat: h2 연동

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.component.Greeting;
+import com.netflix.discovery.converters.Auto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user-service")
 public class UserController {
 
+    private final Greeting greeting;
+
+    @Autowired
+    public UserController(Greeting greeting) {
+        this.greeting = greeting;
+    }
+
     @GetMapping(value = "/health-check")
     public String status() {
         return "It's working in user service";
     }
 
-    @GetMapping(value = "/message")
-    public String message(@RequestHeader("user-request") String header) {
-        log.info("user-service: {}", header);
-        return "Hello world user-service";
+    @GetMapping(value = "/welcome")
+    public String welcome() {
+        return greeting.getMessage();
     }
 
 }
